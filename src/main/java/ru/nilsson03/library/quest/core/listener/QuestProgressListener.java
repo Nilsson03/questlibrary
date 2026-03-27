d8dbbae33e17f4b13ab812813f227a4b33c9beea
Nilsson03 <nilsson_nicholas@mail.ru>
2026-03-27 17:08:09 +0300

    Добавлен QuestProgressListener для обработки событий прогресса квестов

---
+++ src/main/java/ru/nilsson03/library/quest/core/listener/QuestProgressListener.java
@@ -0,0 +1,45 @@
+package ru.nilsson03.library.quest.core.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import ru.nilsson03.library.bukkit.file.configuration.BukkitConfig;
+import ru.nilsson03.library.quest.core.config.Config;
+import ru.nilsson03.library.quest.core.event.UserQuestProgressEvent;
+import ru.nilsson03.library.quest.objective.goal.Goal;
+import ru.nilsson03.library.quest.quest.simple.BaseQuest;
+import ru.nilsson03.library.quest.user.data.QuestUserData;
+import ru.nilsson03.library.text.api.UniversalTextApi;
+import ru.nilsson03.library.text.messeger.UniversalMessenger;
+import ru.nilsson03.library.text.util.ReplaceData;
+
+public class QuestProgressListener implements Listener {
+
+    @EventHandler
+    public void onProgress(UserQuestProgressEvent event) {
+        BaseQuest quest = event.getQuest();
+
+        QuestUserData userData = event.getQuestUserData();
+        Goal goal = event.getGoal();
+
+        long requiredValue = goal.targetValue();
+
+        if (userData == null && quest == null) {
+            return;
+        }
+
+        Player player = Bukkit.getPlayer(userData.uuid());
+
+        if (player == null) {
+            return;
+        }
+
+        long progress = event.getNewValue();
+        UniversalMessenger.send(player, UniversalTextApi.replacePlaceholders(Config.messages_QuestProgress(),
+                new ReplaceData("{quest}", quest.questMeta().displayName()),
+                new ReplaceData("{progress}", progress),
+                new ReplaceData("{required}", requiredValue)));
+    }
+}
